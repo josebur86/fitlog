f105b0fb541faab1ff75b285e0f0de2207fab0b3
Joe Burns <josebur86@gmail.com>
2015-10-21 08:35:28 -0500

    Initial commit.

---
+++ app/src/main/java/com/josebur/fitlog/ExerciseSessionActivity.java
@@ -0,0 +1,13 @@
+package com.josebur.fitlog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_session);
+    }
+}
