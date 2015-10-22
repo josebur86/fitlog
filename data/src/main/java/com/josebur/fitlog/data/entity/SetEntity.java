12532b08f7e734cd724badff67b8015810549585
Joe Burns <josebur86@gmail.com>
2015-10-22 08:42:23 -0500

    Set number cannot be zero or less.

--- data/src/main/java/com/josebur/fitlog/data/entity/SetEntity.java
+++ data/src/main/java/com/josebur/fitlog/data/entity/SetEntity.java
@@ -1,4 +1,39 @@
 package com.josebur.fitlog.data.entity;
 
+/**
+ * Exercise Set entity that is used in the data layer.
+ */
 public class SetEntity {
+    private int setNumber;
+
+    /**
+     * Constructs a set for an exercise
+     * @param setNumber the number of the set to construct.
+     *
+     * @throws IllegalArgumentException if the set number is zero or less
+     */
+    public SetEntity(int setNumber) throws IllegalArgumentException {
+        if (setNumber <= 0 )
+            throw new IllegalArgumentException("set number cannot be zero or less");
+
+        this.setNumber = setNumber;
+    }
+
+    /**
+     * Returns the set number for this set. The set number will determine
+     * the order of the set in a group of sets.
+     * @return the set number
+     */
+    public int getSetNumber() {
+        return setNumber;
+    }
+
+    /**
+     * Returns the string representation of this set
+     * @return string representation
+     */
+    @Override
+    public String toString() {
+        return "Set: " + setNumber;
+    }
 }
--- data/src/test/java/com/josebur/fitlog/data/entity/SetEntityTest.java
+++ data/src/test/java/com/josebur/fitlog/data/entity/SetEntityTest.java
@@ -1,5 +1,47 @@
 package com.josebur.fitlog.data.entity;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
 public class SetEntityTest {
-    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void SetHasANumber() {
+        int setNumber = 1;
+
+        SetEntity set = new SetEntity(setNumber);
+
+        assertEquals(setNumber, set.getSetNumber());
+    }
+
+    @Test
+    public void SetNumberCannotBeZero() {
+        thrown.expect(IllegalArgumentException.class);
+        int setNumber = 0;
+
+        new SetEntity(setNumber);
+    }
+
+    @Test
+    public void SetNumberCannotBeLessThanZero() {
+        thrown.expect(IllegalArgumentException.class);
+        int setNumber = -1;
+
+        new SetEntity(setNumber);
+    }
+
+    @Test
+    public void ToStringIncludesTheSetNumber() {
+        String expected = "Set: 1";
+
+        SetEntity set = new SetEntity(1);
+
+        assertEquals(expected, set.toString());
+    }
 }
