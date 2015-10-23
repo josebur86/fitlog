623ce2e14e14d88e3096dba379d618d0d2f45bd2
Joe Burns <josebur86@gmail.com>
2015-10-23 13:44:33 -0500

    Basic session domain object.

--- domain/src/main/java/com/josebur/fitlog/domain/Session.java
+++ domain/src/main/java/com/josebur/fitlog/domain/Session.java
@@ -1,4 +1,18 @@
-package com.domain;
+package com.josebur.fitlog.domain;
 
 public class Session {
+    private final String exercise;
+    private final int repGoal;
+    public Session(String exercise, int repGoal) {
+        this.exercise = exercise;
+        this.repGoal = repGoal;
+    }
+
+    public String getExercise() {
+        return exercise;
+    }
+
+    public int getRepGoal() {
+        return repGoal;
+    }
 }
--- domain/src/test/java/com/josebur/fitlog/domain/SessionTest.java
+++ domain/src/test/java/com/josebur/fitlog/domain/SessionTest.java
@@ -2,11 +2,23 @@ package com.josebur.fitlog.domain;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class SessionTest {
     @Test
-    public void foo() {
-        assertTrue(true);
+    public void sessionHasAnExercise() {
+        String exercise = "Squat";
+        int repGoal = 5;
+        Session session = new Session(exercise, repGoal);
+
+        assertEquals(exercise, session.getExercise());
+    }
+
+    @Test
+    public void sessionHasARepGoal() {
+        int repGoal = 5;
+        Session session = new Session("squat", repGoal);
+
+        assertEquals(repGoal, session.getRepGoal());
     }
 }
